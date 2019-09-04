cf0892a46982386f40a99116617a7ad0d9a41d30
Robinzon Santana <sant4n4.1@gmail.com>
2019-09-04 17:20:45 -0500

    Ejemplo con data binding de Noticias

--- src/main/java/net/itinajero/app/controller/NoticiasController.java
+++ src/main/java/net/itinajero/app/controller/NoticiasController.java
@@ -1,5 +1,6 @@
 package net.itinajero.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,25 +8,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import net.itinajero.app.model.Noticia;
+import net.itinajero.app.service.INoticiasService;
 
 @Controller
 @RequestMapping("/noticias")
 public class NoticiasController {
-  
+    
+	@Autowired
+	private INoticiasService serviceNoticias;
+	
 	@GetMapping(value = "/create")
 	public String crear() {
 		return "noticias/formNoticia";
 	}
 	
 	@PostMapping(value = "/save")
-	public String guardar(@RequestParam("titulo") String titulo,@RequestParam("estatus") String estatus, @RequestParam("detalle") String detalle) {
-		Noticia objNoticia = new Noticia();
-		objNoticia.setTitulo(titulo);
-		objNoticia.setEstatus(estatus);
-		objNoticia.setDetalle(detalle);
+	//public String guardar(@RequestParam("titulo") String titulo,@RequestParam("estatus") String estatus, @RequestParam("detalle") String detalle) {
+	public String guardar(Noticia noticia) {
 		
-		System.out.println("La Noticia : "+objNoticia);
-        
+		serviceNoticias.guardar(noticia);
+
 		return "noticias/formNoticia";
 		
 	}
---
+++ src/main/java/net/itinajero/app/service/NoticiasServiceImpl.java
@@ -0,0 +1,15 @@
+package net.itinajero.app.service;
+
+import org.springframework.stereotype.Service;
+import net.itinajero.app.model.Noticia;
+
+@Service
+public class NoticiasServiceImpl implements INoticiasService {
+
+	@Override
+	public void guardar(Noticia noticia) {
+		// TODO Auto-generated method stub
+		System.out.println(noticia);
+	}
+
+}
